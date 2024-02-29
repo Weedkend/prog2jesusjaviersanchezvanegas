2a65bc8d10c2599c3f149e2799d1467f1c56736d
Jesus <jesusjsanchez39@gmail.com>
2024-02-29 10:50:30 -0500

    main

---
+++ main.py
@@ -0,0 +1,4 @@
+from Estudiante import Estudiante
+
+p=Estudiante("jesus javier","sanchez vanegas", "1067161619", "jesusjavier.sanchezvanegas@unitecnar.edu.co" , "3205197683")
+print("mi nombre es", p.obtenernombre(), "mi cedula es", p.obtenercedula(), "mi correo es" , p.obtenercorreo() , "y mi numero de telefono es", p.obtenertelefono())
